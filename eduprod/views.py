2cc43db1be7a541dd2911e05560ad4a766df6abe
TamatiMakoare <tamatijp@gmail.com>
2024-03-20 16:50:21 +1100

    authentication

--- eduprod/views.py
+++ eduprod/views.py
@@ -1,8 +1,20 @@
 from django.core import serializers
 from django.shortcuts import render
 from .models import Question
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-    questions = Question.objects.all()
+    # Randomly select two questions
+    questions = Question.objects.order_by('?')[:2]
+    
+    # Serialize the questions to JSON
     questions_json = serializers.serialize('json', questions)
-    return render(request, 'eduprod/index.html', {'questions_json': questions_json})
+
+    # Extracting categories from the Question model (assuming you have defined a category field)
+    categories = Question.objects.values_list('category', flat=True).distinct()
+
+    return render(request, 'eduprod/index.html', {
+        'questions_json': questions_json,
+        'categories': categories
+    })
